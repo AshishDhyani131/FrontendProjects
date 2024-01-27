6f7ab7aa8871bd6923a2e91609af75328611c53b
AshishDhyani131 <ashishdhyani100@gmail.com>
2024-01-27 23:35:32 +0530

    created data.jsx files to store data for display and also created main body of layout

--- Youtube/Youtube/src/App.jsx
+++ Youtube/Youtube/src/App.jsx
@@ -3,15 +3,18 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import SideBar from "./components/Main/SideBar";
-
+import SectionHeader from "./components/Main/SectionHeader";
+import SectionBody from "./components/Main/SectionBody";
+import { sectionHeaderList } from "./data";
 function App() {
   return (
     <div className="container mx-auto mt-4">
       <Header></Header>
-      <main className="flex mt-3">
+      <main className="flex gap-x-16 mt-3">
         <SideBar></SideBar>
-        <section>
-          <SectionHeader></SectionHeader>
+        <section className="w-full">
+          <SectionHeader headerList={sectionHeaderList}></SectionHeader>
+          <SectionBody></SectionBody>
         </section>
       </main>
     </div>
